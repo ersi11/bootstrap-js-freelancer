7031bb21637c9232b0d28de2031d17ee68360ef2
Ersidi Matranxhi <100949686+ersi11@users.noreply.github.com>
2022-03-29 17:49:55 +0200

    Update main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("send").addEventListener("click", calcolaPrezzo);
+
+function calcolaPrezzo(event){
+    event.preventDefault();
+    document.getElementById("Risultato").innerHTML = ("risultatoTotale");
+    
+}
